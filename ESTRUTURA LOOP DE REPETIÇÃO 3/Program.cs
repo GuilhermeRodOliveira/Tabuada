176a76bce83b048ae2b786f8ae9682f11dbff1e8
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-07 14:23:39 -0300

    Adicionar arquivos de projeto.

---
+++ ESTRUTURA LOOP DE REPETIÇÃO 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ESTRUTURA_LOOP_DE_REPETIÇÃO_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 - Crie um programa que peça ao usuário para inserir um número e, em seguida, imprima a tabuada desse número de 1 a 10 usando um loop for.
+
+            Console.WriteLine("Insira um numero para ver a tabuada");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A tabuada de " +numero+ ": ");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
